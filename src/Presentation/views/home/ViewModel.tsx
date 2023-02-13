d79fadb48ca07b61f48148731523b6c5b7cadc8e
Christian George <christiangeorge@MacBook-Air-de-Christian.local>
2023-02-13 08:00:33 -0600

    Creación de los métodos de getUser para el async storage de nuestro proyecto

--- src/Presentation/views/home/ViewModel.tsx
+++ src/Presentation/views/home/ViewModel.tsx
@@ -1,5 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { LoginAuthUseCase } from '../../../Domain/useCases/auth/LoginAuth';
+import { SaveUserUseCase } from '../../../Domain/useCases/userLocal/SaveUser';
+import { GetUserUseCase } from '../../../Domain/useCases/userLocal/GetUser';
 
 const HomeViewModel = () => {
   const [errorMessage, setErrorMessage] = useState('');
@@ -7,6 +9,16 @@ const HomeViewModel = () => {
     email: '',
     password: '',
   });
+
+  useEffect(() => {
+    getUserSession();
+  }, [])
+
+  const getUserSession = async () => {
+    const user = await GetUserUseCase();
+    console.log('USUARIO SESIÓN' + JSON.stringify(user));
+  }
+  
 
   const onChange = (property: string, value: any) => {
     setValues({ ...values, [property]: value })
@@ -19,6 +31,8 @@ const HomeViewModel = () => {
 
       if(!response.success) {
         setErrorMessage(response.message);
+      } else {
+        await SaveUserUseCase(response.data);
       }
     }
   }
